46789f4202060af4ffcccc646e5e8708dd0a8f32
Maksim <m.shumkov@einmahl-works.de>
2026-04-10 15:15:53 +0200

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: [
-    '@nuxt/image',
-    '@pinia/nuxt',
-    '@nuxtjs/tailwindcss',
-    'nuxt-swiper'
-  ]
-})
+  app: {
+    baseURL: "/pizziria/",
+  },
+  modules: ["@nuxt/image", "@pinia/nuxt", "@nuxtjs/tailwindcss", "nuxt-swiper"],
+});
